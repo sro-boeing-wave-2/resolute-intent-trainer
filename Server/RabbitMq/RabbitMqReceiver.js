fab1e06d4c64c12c6d347bc6e4e1737f45d272c2
vasamsettisaikiran <vasamsettisaikiran@yahoo.com>
2018-10-03 21:12:53 +0530

    modified rabbitMq consumer

--- Server/RabbitMq/RabbitMqReceiver.js
+++ Server/RabbitMq/RabbitMqReceiver.js
@@ -1,4 +1,4 @@
-const { USER_SLUG, BOT_SLUG, RABBITMQ_QUEUE,RECAST_AUTHORIZATION } = require('./app.config');
+const { USER_SLUG, BOT_SLUG, RABBITMQ_QUEUE, RECAST_AUTHORIZATION } = require('./app.config');
 const functions = require('./RecastFunctions');
 const amqplib = require('amqplib');
 const request = require('superagent');
@@ -15,30 +15,35 @@ open.then(function (conn) {
     return ch.consume(RABBITMQ_QUEUE, function (msg) {
       if (msg !== null) {
         var data = JSON.parse(msg.content);
-        request
-        .get(`https://api.recast.ai/v2/users/${USER_SLUG}/bots/${BOT_SLUG}/intents`)
-        .send()
-        .set('Authorization', RECAST_AUTHORIZATION)
-        .end((err, res) => {
-            data = JSON.parse(res.text);
-            recastData = [];
-            [data.results.forEach(element => {
+        console.log("Message from Queue - ");
+        console.log(data);
+        if (data.Intent != null) {
+          request
+            .get(`https://api.recast.ai/v2/users/${USER_SLUG}/bots/${BOT_SLUG}/intents`)
+            .send()
+            .set('Authorization', RECAST_AUTHORIZATION)
+            .end((err, res) => {
+              data = JSON.parse(res.text);
+              recastData = [];
+              [data.results.forEach(element => {
                 recastData.push(element.name)
+              })
+              ];
             })
-            ];
-        })
-        console.log(recastData);
-        if (recastData.lastIndexOf(data.intent) == -1) {
-          functions.
-            CreateIntent(USER_SLUG, BOT_SLUG, data.intent, data.description, data.expressions); 
-          ch.ack(msg);
+          console.log(recastData);
+          if (recastData.lastIndexOf(data.intent) == -1) {
+            functions.
+              CreateIntent(USER_SLUG, BOT_SLUG, data.Intent, data.Description, data.Description);
+
+          }
+          else {
+            console.log(data.intent);
+            functions.
+              AddToIntent(USER_SLUG, BOT_SLUG, data.Description, data.Intent.toLowerCase())
+
+          }
         }
-        else {
-          console.log(data.intent);
-          functions.
-            AddToIntent(USER_SLUG, BOT_SLUG, data.expressions[0], data.intent.toLowerCase())     
-          ch.ack(msg);
-        }
+        ch.ack(msg);
       }
     });
   });
